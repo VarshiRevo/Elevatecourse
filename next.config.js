c1fc156cb733a4596e006ee052069524273b11c3
Varshiga P S <145317472+VarshiRevo@users.noreply.github.com>
2025-02-03 00:34:49 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
 const nextConfig = {
   images: {
     domains: ['cdn.builder.io', 'res.cloudinary.com', 'randomuser.me'],
     dangerouslyAllowSVG: true, // Enable SVG support
     contentSecurityPolicy: "default-src 'self'; img-src *; media-src *; script-src 'none'; sandbox;",
-  },
-  experimental: {
-      // Remove unsupported properties
   }
-}
+};
 
 module.exports = nextConfig;
