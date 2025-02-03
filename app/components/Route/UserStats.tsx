9d4a1d3b830852875232baa46d0780470702872d
Varshiga P S <145317472+VarshiRevo@users.noreply.github.com>
2025-02-03 13:58:39 +0530

    Update UserStats.tsx

--- app/components/Route/UserStats.tsx
+++ app/components/Route/UserStats.tsx
@@ -3,7 +3,7 @@ import { UserStatProps } from './types';
 
 export const UserStats: React.FC<UserStatProps> = ({ count, label, imageSrc }) => {
   return (
-    <div className="flex gap-3 items-end self-stretch my-auto text-black min-w-[240px]">
+    <div className="flex gap-3 items-end self-stretch my-auto text-black min-w-[240px] dark:text-white">
       <img loading="lazy" src={imageSrc} alt="User avatars" className="object-contain shrink-0 aspect-[2.5] w-[155px]" />
       <div className="flex flex-col min-h-[64px] w-[136px]">
         <h3 className="text-3xl font-bold">{count}</h3>
