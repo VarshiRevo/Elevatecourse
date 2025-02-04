aa534ff3f6b32f9117a0fd3321ec0362de6b22b7
Varshiga P S <145317472+VarshiRevo@users.noreply.github.com>
2025-02-04 09:30:34 +0530

    Update FooterSection.tsx

--- app/components/FooterSection.tsx
+++ app/components/FooterSection.tsx
@@ -2,7 +2,7 @@ import { FooterSectionProps } from './types';
 
 export const FooterSection: React.FC<FooterSectionProps> = ({ title, links }) => (
   <section className="flex flex-col grow shrink min-w-[240px]">
-    <h3 className="text-2xl font-bold leading-none text-neutral-900">
+    <h3 className="text-2xl font-bold leading-none text-neutral-900 dark:text-white">
       {title}
     </h3>
     <nav className="mt-2.5 text-2xl tracking-wide leading-[66px] text-zinc-600">
