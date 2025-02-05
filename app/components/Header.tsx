fba268684b4e057ec3d6951e84b0b656e7655121
Varshiga P S <145317472+VarshiRevo@users.noreply.github.com>
2025-02-05 14:35:42 +0530

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,234 +1,134 @@
-"use client";
-import Link from "next/link";
-import React, { FC, useEffect, useState } from "react";
-import NavItems from "../utils/NavItems";
-import { ThemeSwitcher } from "../utils/ThemeSwitcher";
-import { HiOutlineMenuAlt3, HiOutlineUserCircle } from "react-icons/hi";
-import CustomModal from "../utils/CustomModal";
-import Login from "../components/Auth/Login";
-import SignUp from "../components/Auth/SignUp";
-import Verification from "../components/Auth/Verification";
+'use client'
+import { useGetHeroDataQuery } from "@/redux/features/layout/layoutApi";
 import Image from "next/image";
-import avatar from "../../public/assests/avatar.png";
-import { useSession } from "next-auth/react";
-import { useLogOutQuery, useSocialAuthMutation } from "@/redux/features/auth/authApi";
-import { toast } from "react-hot-toast";
-import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
-import Loader from "./Loader/Loader";
+import Link from "next/link";
+import React, { FC, useState } from "react";
+import { BiSearch } from "react-icons/bi";
+import Loader from "../Loader/Loader";
+import { useRouter } from "next/navigation";
+import { CTAButton } from './CTAButton';
+import { UserStats } from './UserStats';
+import './im.css';
+type Props = {};
 
-type Props = {
-  open: boolean;
-  setOpen: (open: boolean) => void;
-  activeItem: number;
-  route: string;
-  setRoute: (route: string) => void;
-};
+const Hero: FC<Props> = (props) => {
+  const { data, isLoading } = useGetHeroDataQuery("Banner", {});
+  const [search, setSearch] = useState("");
+  const router = useRouter()
 
-const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
-  const [active, setActive] = useState(false);
-  const [openSidebar, setOpenSidebar] = useState(false);
-  const { data: userData, isLoading, refetch } = useLoadUserQuery(undefined, {});
-  const { data } = useSession();
-  const [socialAuth, { isSuccess, error }] = useSocialAuthMutation();
-  const [logout, setLogout] = useState(false);
-  const { } = useLogOutQuery(undefined, {
-    skip: !logout ? true : false,
-  });
-
-  useEffect(() => {
-    if (!isLoading) {
-      if (!userData) {
-        if (data) {
-          socialAuth({
-            email: data?.user?.email,
-            name: data?.user?.name,
-            avatar: data.user?.image,
-          });
-          refetch();
-        }
-      }
-      if (data === null) {
-        if (isSuccess) {
-          toast.success("Login Successfully");
-        }
-      }
-      if (data === null && !isLoading && !userData) {
-        setLogout(true);
-      }
+  const handleSearch = () => {
+    if (search === "") {
+      return
+    } else {
+      router.push(`/courses?title=${search}`);
     }
-  }, [data, userData, isLoading]);
-
-  if (typeof window !== "undefined") {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 85) {
-        setActive(true);
-      } else {
-        setActive(false);
-      }
-    });
   }
 
-  const handleClose = (e: any) => {
-    if (e.target.id === "screen") {
-      {
-        setOpenSidebar(false);
-      }
-    }
-  };
 
   return (
     <>
-      {
-        isLoading ? (
-          <Loader />
-        ) : (
-          <div className="w-full relative">
-            <div
-              className={`${active
-                ? "dark:bg-opacity-50 bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
 
-                : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
-                }`}
-            >
-              <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
-                <div className="w-full h-[80px] flex items-center justify-between p-3">
-                  <div>
-                    <Link
-                      href={"/"}
-                      className={`text-[25px] font-[500] text-black dark:text-white flex items-center gap-2`}
-                    >
-                      <div className="flex gap-2.5 items-center self-stretch my-auto text-3xl font-black dark:text-white  text-black whitespace-nowrap">
-                        <img
-                          loading="lazy"
-                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/52b36090a44447a86a77abc7580c9213dc2ab96488c69450f72f927c385a15e1?placeholderIfAbsent=true&apiKey=c48d9b50468c496f834245b48efcd26e"
-                          alt="Levate logo"
-                          className="object-contain shrink-0 self-stretch my-auto aspect-[1.3] w-[87px]"
-                        />
-                        <span className="self-stretch my-auto ">Levate</span>
-                      </div>
-                    </Link>
-                  </div>
-                  <div className="flex items-center">
-                    <NavItems activeItem={activeItem} isMobile={false} />
+      <section className="flex overflow-hidden flex-col justify-center pt-10 pb-20 w-full max-md:max-w-full sm:px-6 lg:px-0">
 
-                    {/* only for mobile */}
-                    <div className="800px:hidden flex items-center justify-between gap-4">
-                      {userData ? (
-                        <Link href={"/profile"}>
-                          <Image
-                            src={userData?.user.avatar ? userData.user.avatar.url : avatar}
-                            alt="Profile"
-                            width={30}
-                            height={30}
-                            className="w-[30px] h-[30px] rounded-full cursor-pointer"
-                            style={{ border: activeItem === 5 ? "2px solid #37a39a" : "none" }}
-                          />
-                        </Link>
-                      ) : (
-                        <HiOutlineUserCircle
-                          size={25}
-                          className="cursor-pointer text-black dark:text-white"
-                          onClick={() => setOpen(true)}
-                        />
-                      )}
-                      <HiOutlineMenuAlt3
-                        size={25}
-                        className="cursor-pointer dark:text-white text-black"
-                        onClick={() => setOpenSidebar(true)}
-                      />
-                    </div>
+        <div className="flex flex-col self-center w-full max-w-[1276px] px-4 sm:px-6 lg:px-0">
 
-                    {userData ? (
-                      <Link href={"/profile"} >
-                        <Image
-                          src={userData?.user.avatar ? userData.user.avatar.url : avatar}
-                          alt=""
-                          width={30}
-                          height={30}
-                          className="hidden 800px:block w-[30px] h-[30px] rounded-full cursor-pointer"
-                          style={{ border: activeItem === 5 ? "2px solid #37a39a" : "none" }}
-                        />
-                      </Link>
-                    ) : (
-                      <HiOutlineUserCircle
-                        size={25}
-                        className="hidden 800px:block cursor-pointer dark:text-white text-black"
-                        onClick={() => setOpen(true)}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
+          <header className="flex flex-col justify-center w-full max-md:max-w-full">
+            <div className="flex flex-col w-full max-w-9xl px-4 sm:px-6 lg:px-20">
 
-              {/* mobile sidebar */}
-              {openSidebar && (
-                <div
-                  className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-gray-950 bg-[#00000024]"
-                  onClick={handleClose}
-                  id="screen"
-                >
-                  <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-gray-900 dark:bg-opacity-95 top-0 right-0 shadow-lg dark:shadow-gray-800">
+              <h1 className="text-5xl sm:text-6xl md:text-7xl font-black  tracking-tighter leading-none text-black bg-clip-text bg-[linear-gradient(90deg,#EA728D_67.21%,#EF9EAE_71.33%,#ECCD43_81.49%,#42C3E5_85.63%)] max-md:max-w-full max-md:text-3xl">
 
-                    <NavItems activeItem={activeItem} isMobile={true} />
-                    
+                <span className="dark:text-white">Master New Skills with</span> <span className="text-transparent bg-clip-text "style={{
+                  backgroundImage:
+                    'linear-gradient(90deg,#EA5BEB,#4B57F5)',
+                }}>Elevate</span>🚀
+              </h1>
+              <p className="mt-6 text-lg sm:text-xl font-medium capitalize text-neutral-500 max-md:max-w-full">
 
-                    <br />
-                    <br />
-                    <p className="text-[16px] px-2 pl-5 text-black dark:text-gray-300">
-
-                      Copyright © 2024 Elevate
-                    </p>
-                  </div>
-                </div>
-              )}
+                With expert instructors and interactive content, with high-quality learning experience all levels,
+                access personalized learning paths and achieve your educational goals.
+              </p>
             </div>
-            {route === "Login" && (
-              <>
-                {open && (
-                  <CustomModal
-                    open={open}
-                    setOpen={setOpen}
-                    setRoute={setRoute}
-                    activeItem={activeItem}
-                    component={Login}
-                    refetch={refetch}
-                  />
-                )}
-              </>
-            )}
+          </header>
 
-            {route === "Sign-Up" && (
-              <>
-                {open && (
-                  <CustomModal
-                    open={open}
-                    setOpen={setOpen}
-                    setRoute={setRoute}
-                    activeItem={activeItem}
-                    component={SignUp}
-                  />
-                )}
-              </>
-            )}
+          <div className="flex flex-wrap gap-6 sm:gap-8 items-center px-4 sm:px-6 lg:px-20 self-start mt-8 max-md:max-w-full">
 
-            {route === "Verification" && (
-              <>
-                {open && (
-                  <CustomModal
-                    open={open}
-                    setOpen={setOpen}
-                    setRoute={setRoute}
-                    activeItem={activeItem}
-                    component={Verification}
+            <div className="flex items-center border rounded-full p-2 w-full max-w-lg sm:w-[400px]">
+
+              <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
+                <input
+                  type="search"
+                  placeholder="Search Courses..."
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="bg-transparent dark:border-none  dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-black dark:text-white text-lg sm:text-xl font-medium"
+                />
+
+              </div>
+              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-500 cursor-pointer hover:bg-blue-600 transition" onClick={handleSearch}>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-4.35-4.35m1.85-5.15a7 7 0 11-14 0 7 7 0 0114 0z"
                   />
-                )}
-              </>
-            )}
+                </svg>
+              </div>
+            </div>
+
+            <Link href="/courses">
+              <CTAButton
+                text="Get Started"
+                iconSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/d12551e95d461b3d630ded4505aa6ec1dbd52d633ed06a0fd76d7ab6eb6dc2f0?placeholderIfAbsent=true&apiKey=c48d9b50468c496f834245b48efcd26e"
+                variant="solid"
+              />
+            </Link>
+            <UserStats
+              count="12k+"
+
+              label="using the app"
+              imageSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/448cdd46043dbb9ed3ffee86b2ac2efb46420fc0b02253f137970a4375f60f2b?placeholderIfAbsent=true&apiKey=c48d9b50468c496f834245b48efcd26e"
+            />
           </div>
-        )
-      }
+        </div>
+        <div className="image-container flex flex-col sm:flex-row gap-6 sm:gap-10 items-center justify-center mt-10">
+
+          <div className="moving-images">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/66449b53aa95c461bf0907515a594484e525c305526760133c1eab2fa339bd57?apiKey=c48d9b50468c496f834245b48efcd26e&"
+              alt="Quiz platform illustration - 1"
+              className="image"
+
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/66449b53aa95c461bf0907515a594484e525c305526760133c1eab2fa339bd57?apiKey=c48d9b50468c496f834245b48efcd26e&"
+              alt="Quiz platform illustration -2"
+              className="image"
+            />
+          </div>
+          <div className="blur-left"></div>
+          <div className="blur-right"></div>
+        </div>
+        {/* <div className="flex flex-col mt-16 w-full px-10 max-md:mt-10 max-md:max-w-full">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/92d90dba1a3127921c44016c5233b7a7fd72c610c9d8e4931aae7d1f963021a6?placeholderIfAbsent=true&apiKey=c48d9b50468c496f834245b48efcd26e"
+          alt="Company logos ticker"
+          className="object-contain w-full aspect-[3.47] max-md:max-w-full"
+        />
+      </div> */}
+      </section>
     </>
   );
 };
 
-export default Header;
+export default Hero;
